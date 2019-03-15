ba065b183fb099554ddec4f2dddebc6f424d8117
彭子修 <pengzixiu@pengzixiudeMacBook-Air.local>
2019-03-15 17:22:07 +0800

    save

--- client/pages/concurrent-request.jsx
+++ client/pages/concurrent-request.jsx
@@ -12,7 +12,7 @@ const Container = styled.div`
   min-height: 100vh;
   font-size: 1rem;
 
-  button{
+  button {
     margin: 20px;
   }
 `;
@@ -22,55 +22,78 @@ export default function ConcurrentRequest() {
   const urls = Array(20).fill("http://api.tvmaze.com/search/shows?q=batman");
 
   async function secondary() {
-    try{
-        let shows = [];
-        for(const url of urls){
-            const res = await fetch(url);
-            const res_to_json = await res.json();
-            shows.push(res_to_json);
-        };
-        const show_list = shows.reduce((pre, now)=>{
-            return pre.concat(now)
-        });
-        setShow(show_list);
-    }catch(e){
-        throw e
+    try {
+      let shows = [];
+      for (const url of urls) {
+        const res = await fetch(url);
+        const res_to_json = await res.json();
+        shows.push(res_to_json);
+      }
+      const show_list = shows.reduce((pre, now) => {
+        return pre.concat(now);
+      });
+      setShow(show_list);
+    } catch (e) {
+      throw e;
     }
   }
 
-  async function concurrent(){
-    try{
-        const maxLimit = 5;
-        const current = 0;
-        for(const url of urls){
-            if(current < maxLimit){
-                return await fetch(url)
-            }
-        }
-    }catch(e){
-        throw e
+  function concurrent(urls, limit, callback) {
+    function* format_urls() {
+      for (const url of urls) {
+        yield fetch(url).then(res => res.json());
+      }
     }
+    const g = format_urls();
+    let current = 0;
+    let list = [];
+
+    return function loop() {
+      if (current > limit) {
+        return;
+      }
+      current++
+      console.log('current: ', current);
+      const { value, done } = g.next();
+      
+      if (done) {
+        console.log("finished!", list);
+        return;
+      }
+      value.then(res => {
+        list.push(res);
+        current--;
+      });
+    };
   }
 
   const reset = () => setShow([]);
+
+  function callback(res) {
+    console.log(res);
+    setShow(res);
+  }
 
   return (
     <Container>
       This is Concurent request page!
       <div>
-            <Button onClick={secondary}>secondary request</Button>
-            <Button onClick={concurrent}>concurrent request</Button>
-            <Button onClick={reset}>reset</Button>
+        <Button onClick={secondary}>secondary request</Button>
+        <Button onClick={() => concurrent(urls, 5, callback)()}>
+          concurrent request
+        </Button>
+        <Button onClick={reset}>reset</Button>
       </div>
       <List
         size="small"
         header={<div>list</div>}
-        style={{width: '80vw'}}
+        style={{ width: "80vw" }}
         bordered
         dataSource={show}
         renderItem={item => (
           <List.Item>
-            {item.show.name} <img style={{height: '5rem'}} src={item.show.image.medium} />
+            {item.show.name}{" "}
+            <img style={{ height: "5rem" }} src={item.show.image.medium} />
           </List.Item>
         )}
       />
