542e2fe558d2126d7de6efe6d316f63aaf013531
彭子修 <pengzixiu@pengzixiudeMacBook-Air.local>
2019-03-15 15:17:40 +0800

    save

--- client/pages/concurrent-request.jsx
+++ client/pages/concurrent-request.jsx
@@ -1,28 +1,79 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { List, Button } from "antd";
+import styled from "styled-components";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    width: 100vw;
-    min-height: 100vh;
-    font-size: 1rem;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 100vw;
+  min-height: 100vh;
+  font-size: 1rem;
 
-export default function ConcurrentRequest (){
+  button{
+    margin: 20px;
+  }
+`;
 
-    const [count, setCount] = useState(0);
-    
-    return (
-        <Container>
-            This is Concurent request page!
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
-        </Container>
-    )
+export default function ConcurrentRequest() {
+  const [show, setShow] = useState([]);
+  const urls = Array(20).fill("http://api.tvmaze.com/search/shows?q=batman");
+
+  async function secondary() {
+    try{
+        let shows = [];
+        for(const url of urls){
+            const res = await fetch(url);
+            const res_to_json = await res.json();
+            shows.push(res_to_json);
+        };
+        const show_list = shows.reduce((pre, now)=>{
+            return pre.concat(now)
+        });
+        setShow(show_list);
+    }catch(e){
+        throw e
+    }
+  }
+
+  async function concurrent(){
+    try{
+        const maxLimit = 5;
+        const current = 0;
+        for(const url of urls){
+            if(current < maxLimit){
+                return await fetch(url)
+            }
+        }
+    }catch(e){
+        throw e
+    }
+  }
+
+  const reset = () => setShow([]);
+
+  return (
+    <Container>
+      This is Concurent request page!
+      <div>
+            <Button onClick={secondary}>secondary request</Button>
+            <Button onClick={concurrent}>concurrent request</Button>
+            <Button onClick={reset}>reset</Button>
+      </div>
+      <List
+        size="small"
+        header={<div>list</div>}
+        style={{width: '80vw'}}
+        bordered
+        dataSource={show}
+        renderItem={item => (
+          <List.Item>
+            {item.show.name} <img style={{height: '5rem'}} src={item.show.image.medium} />
+          </List.Item>
+        )}
+      />
+    </Container>
+  );
 }
